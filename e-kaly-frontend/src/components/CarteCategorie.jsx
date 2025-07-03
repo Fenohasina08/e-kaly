01590217ddc9096ff7269b9a25f3609712ae77eb
herimamy <fenohasinaherimamy@gmail.com>
2025-07-03 16:31:12 +0000

    finalisation des cartes de catégories

--- e-kaly-frontend/src/components/CarteCategorie.jsx
+++ e-kaly-frontend/src/components/CarteCategorie.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-
-export default function CarteCategorie({ titre, image }) {
+ import React from "react";
+ export default function CarteCategorie({ titre, image }) {
   return (
-    <div className="p-4 transition-transform bg-white rounded-lg shadow-md cursor-pointer hover:scale-105">
-      <img src={image} alt={titre} className="object-cover w-full h-32 rounded-md" />
-      <h3 className="mt-2 text-xl font-bold text-center">{titre}</h3>
+    <div className="flex flex-col w-[450px] h-[300px] sm:w-[280px] sm:h-[330px] md:w-[300px] md:h-[350px] lg:w-[320px] lg:h-[380px] 
+                bg-white rounded-xl shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+
+      {/* Image : 80% de la hauteur */}
+      <div className="w-full h-4/5">
+        <img src={image} alt={titre} className="object-cover w-full h-full" />
+      </div>
+
+      {/* Texte : 20% de la hauteur */}
+      <div className="flex items-center justify-center w-full bg-white h-1/5">
+        <h3 className="text-xl font-bold text-center text-gray-800">{titre}</h3>
+      </div>
+
     </div>
   );
 }
--- e-kaly-frontend/src/components/Plat.jsx
+++ e-kaly-frontend/src/components/Plat.jsx
@@ -1,27 +1,36 @@
 import React from "react";
-import CarteCategorie from "./CarteCategorie";
+import CarteCategorie from "./CarteCategorie.jsx";
 
 export default function Plat() {
     const categories = [
-  { titre: 'Plats traditionnels', image: '/images/romazava.jpg' },
-  { titre: 'Volaille & Viandes', image: '/images/viande.jpg' },
-  { titre: 'Snacks & Street-food', image: '/images/snack.jpg' },
-  { titre: 'Desserts', image: '/images/dessert.jpg' },
-  { titre: 'Accompagnements', image: '/images/lasary.jpg' },
-  { titre: 'Plats végétariens', image: '/images/vegan.jpg' },
+  { titre: 'Plats traditionnels', image: './images/plattraditionnel.jpg' },
+  { titre: 'Volaille & Viandes', image: './images/Volaillesetviandes.webp' },
+  { titre: 'Snacks & Street-food', image: './images/snacks.jpg' },
+  { titre: 'Desserts', image: './images/desserts.jpg' },
+  { titre: 'Plats végétariens', image: './images/Plat_végétarien.jpg' },
 ];
 
     
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h2 className="text-2xl text-center">Ce que nous offrons</h2>
-        
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-        {categories.map((cat, index) => (
-          <CarteCategorie key={index} titre={cat.titre} image={cat.image} />
-        ))}
-      </div>
+      <div className="flex flex-col gap-5 bg-gray-100">
+        <div className="flex justify-center mt-6">
+          <button className="px-6 py-2 font-semibold text-black bg-yellow-500 rounded-full shadow transitackion-colors hover:bg-yellow-600">
+            Voir toutes les catégories
+          </button>
         </div>
+
+        <div className="flex flex-row items-center justify-center gap-8 px-4 overflow-x-auto">
+          {categories.map((cat, index) => (
+            <CarteCategorie
+              key={index}
+              titre={cat.titre}
+              image={cat.image}
+              className="w-[250px] flex-shrink-0"
+            />
+                ))}
+        </div>
+    </div>
+
 
         
 
