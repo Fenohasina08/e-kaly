f43d76e807d40f83e4d5c374888a2840bae445ed
herimamy <fenohasinaherimamy@gmail.com>
2025-07-03 14:27:07 +0000

    Finalisation de l'header et la créations des cartes de categories(version 1)

--- e-kaly-frontend/src/App.jsx
+++ e-kaly-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Header from './components/Header'
-
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Plat from './components/Plat';
 function App() {
   return (
     <>
       <Header />
-      
+      <Hero />
+      <Plat />
     </>
   )
 }
